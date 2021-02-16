994f57f03c4564f5fde3bf878cf31113da4eeb6f
James Bliss <james.bliss@outlook.com>
2021-02-16 16:31:58 +0000

    RemoveDuplicates tests and empty class

---
+++ src/lib/Arrays/RemoveDuplicates.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace lib.Arrays
+{
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/fun-with-arrays/526/deleting-items-from-an-array/3248/
+    /// </summary>
+    public class RemoveDuplicates
+    {
+        public int Go(int[] nums)
+        {
+
+        }
+    }
+}
---
+++ test/Arrays/RemoveDuplicates_UnitTests.cs
@@ -0,0 +1,44 @@
+using System.Runtime;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using System;
+using Xunit;
+using lib.Arrays;
+
+namespace test.Arrays
+{
+    public class RemoveDuplicates_UnitTests
+    {
+        [Fact]
+        public void SimpleTest_SingleRemoval()
+        {
+            //Given
+            var nums = new int[3] { 1, 1, 2 };
+            
+            //When
+            var result = new RemoveDuplicates().Go(nums);
+
+            //Then
+            Assert.Equal(2, result);
+            Assert.Equal(nums[0], 1); 
+            Assert.Equal(nums[1], 2); 
+        }
+
+        public void SimpleTest_MultipleRemoval()
+        {
+            //Given
+            var nums = new int[10] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            
+            //When
+            var result = new RemoveDuplicates().Go(nums);
+
+            //Then
+            Assert.Equal(5, result);
+            Assert.Equal(nums[0], 0); 
+            Assert.Equal(nums[1], 1); 
+            Assert.Equal(nums[2], 2); 
+            Assert.Equal(nums[3], 3); 
+            Assert.Equal(nums[4], 4); 
+        }
+    }
+}
