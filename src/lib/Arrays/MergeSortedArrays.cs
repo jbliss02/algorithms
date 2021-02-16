00d8368756be4c75ea3b286ea2e9f60fe30923ca
James Bliss <james.bliss@outlook.com>
2021-02-16 14:15:48 +0000

    Solution for MergedSortedArrays that uses another array to copy to.

--- src/lib/Arrays/MergeSortedArrays.cs
+++ src/lib/Arrays/MergeSortedArrays.cs
@@ -9,7 +9,38 @@ namespace lib.Arrays
         /// </summary>
         public void Go(int[] nums1, int m, int[] nums2, int n)
         {
+            var result = new int[nums1.Length];
 
+            var array1 = 0;
+            var array2 = 0;
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (array1 == m)
+                {
+                    result[i] = nums2[array2];
+                    array2++;
+                    continue;
+                }
+                else if (array2 == n)
+                {
+                    result[i] = nums1[array1];
+                    array1++;
+                    continue;
+                }
+                    
+                if (nums1[array1] < nums2[array2])
+                {
+                    result[i] = nums1[array1];
+                    array1++;
+                }
+                else {
+                    result[i] = nums2[array2];
+                    array2++;
+                }
+            }
+
+            Array.Copy(result, nums1, nums1.Length);
         }
     }
 }
--- test/Arrays/MergeSortedArrays_UnitTests.cs
+++ test/Arrays/MergeSortedArrays_UnitTests.cs
@@ -28,5 +28,56 @@ namespace test.Arrays
             var expected = new int[6] { 1, 2, 3, 4, 5, 6 };
             Assert.Equal(expected, nums1);
         }
+
+        [Fact]
+        public void SimpleTest_OneEmptyArray()
+        {
+            //Given
+            var nums1 = new int[1] { 1 };
+            var nums2 = new int[0];
+            var m = 1;
+            var n = 0;
+            
+            //When
+            new MergeSortedArrays().Go(nums1, m, nums2, n);
+
+            //Then
+            var expected = new int[1] { 1 };
+            Assert.Equal(expected, nums1);
+        }
+
+        [Fact]
+        public void SimpleTest_NonCrossOverArrays()
+        {
+            //Given
+            var nums1 = new int[9] { 1, 3, 5, 9, 12, 15, 0, 0, 0 };
+            var nums2 = new int[3] { 22, 34, 36 };
+            var m = 6;
+            var n = 3;
+            
+            //When
+            new MergeSortedArrays().Go(nums1, m, nums2, n);
+
+            //Then
+            var expected = new int[9] { 1, 3, 5, 9, 12, 15, 22, 34, 36 };
+            Assert.Equal(expected, nums1);
+        }
+        
+        [Fact]
+        public void SimpleTest_NonCrossOverArrays_Backwards()
+        {
+            //Given
+            var nums1 = new int[9] { 21, 23, 25, 39, 412, 515, 0, 0, 0 };
+            var nums2 = new int[3] { 2, 4, 6 };
+            var m = 6;
+            var n = 3;
+            
+            //When
+            new MergeSortedArrays().Go(nums1, m, nums2, n);
+
+            //Then
+            var expected = new int[9] { 2, 4, 6, 21, 23, 25, 39, 412, 515 };
+            Assert.Equal(expected, nums1);
+        }
     }
 }
