5cec007ce40ef8ac5331bb75d817a8a04b35f0c1
James Bliss <james.bliss@outlook.com>
2021-02-16 13:04:58 +0000

    Added DuplicateZeros, moved existing classes to src/lib/Arrays

---
+++ src/lib/Arrays/DuplicateZeros.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace src.lib.Arrays
+{
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/fun-with-arrays/525/inserting-items-into-an-array/3245/
+    /// </summary>
+    public class DuplicateZeros
+    {
+        
+    }
+}
---
+++ test/Arrays/DuplicateZeros_UnitTests.cs
@@ -0,0 +1,25 @@
+using System.Runtime;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using System;
+using Xunit;
+using lib;
+
+namespace test.Arrays
+{
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/fun-with-arrays/525/inserting-items-into-an-array/3245/
+    /// </summary>
+    public class DuplicateZeros_UnitTests
+    {
+        [Fact]
+        public void SimpleTest_()
+        {
+        //Given
+        
+        //When
+        
+        //Then
+        }
+    }
+}
