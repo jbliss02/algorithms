b0c7e13f09209105ac73cda2d225a229f74cb2c7
James Bliss <james.bliss@outlook.com>
2021-02-16 13:52:49 +0000

    DuplicateZeros solution

--- src/lib/Arrays/DuplicateZeros.cs
+++ src/lib/Arrays/DuplicateZeros.cs
@@ -1,12 +1,47 @@
 using System;
 
-namespace src.lib.Arrays
+namespace lib.Arrays
 {
     /// <summary>
     /// https://leetcode.com/explore/learn/card/fun-with-arrays/525/inserting-items-into-an-array/3245/
     /// </summary>
     public class DuplicateZeros
     {
-        
+        public void Go(int[] nums)
+        {
+            int index = 0;
+
+            while (index < nums.Length - 1)
+            {
+                if (nums[index] == 0)
+                {
+                    for (int i = nums.Length - 2; i > index; i--)
+                    {
+                        nums[i + 1] = nums[i];
+                    }
+
+                    nums[index + 1] = 0;
+
+                    index++;
+                }
+                index++;
+            }
+        }
+
+        public void SimpleBackwardsIteration(int[] nums)
+        {
+            for (int i = nums.Length - 2; i >= 0; i--)
+            {
+                if (nums[i] == 0)
+                {
+                    for (int k = nums.Length - 2; k > i; k--)
+                    {
+                        nums[k + 1] = nums[k];
+                    }
+
+                    nums[i + 1] = 0;
+                }              
+            }
+        }
     }
 }
--- test/Arrays/DuplicateZeros_UnitTests.cs
+++ test/Arrays/DuplicateZeros_UnitTests.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 using System.Reflection;
 using System;
 using Xunit;
-using lib;
+using lib.Arrays;
 
 namespace test.Arrays
 {
@@ -13,13 +13,85 @@ namespace test.Arrays
     public class DuplicateZeros_UnitTests
     {
         [Fact]
-        public void SimpleTest_()
+        public void SimpleTest_SomeZeros()
         {
-        //Given
-        
-        //When
-        
-        //Then
+            var nums = new int[8] { 1, 0, 2, 3, 0, 4, 5, 0 };
+            new DuplicateZeros().Go(nums);
+
+            var result = new int[8] { 1, 0, 0, 2, 3, 0, 0, 4 };
+            Assert.Equal(nums, result);
         }
+
+        [Fact]
+        public void SimpleTest_NoZeros()
+        {
+            var nums = new int[3] { 1, 2, 3};
+            new DuplicateZeros().Go(nums);
+
+            var result = new int[3] { 1, 2, 3};
+            Assert.Equal(nums, result);
+        }
+
+        [Fact]
+        public void EdgeTest_StartsWithZeros()
+        {
+            var nums = new int[10] { 0, 1, 0, 2, 3, 0, 4, 5, 0, 8 };
+            new DuplicateZeros().Go(nums);
+
+            var result = new int[10] { 0, 0, 1, 0, 0, 2, 3, 0, 0, 4 };
+            Assert.Equal(nums, result);
+        }
+
+        [Fact]
+        public void EdgeTest_EndsWithZeros()
+        {
+            var nums = new int[10] { 0, 1, 0, 2, 3, 0, 4, 5, 0, 0 };
+            new DuplicateZeros().Go(nums);
+
+            var result = new int[10] { 0, 0, 1, 0, 0, 2, 3, 0, 0, 4 };
+            Assert.Equal(nums, result);
+        }
+
+        [Fact]
+        public void EdgeTest_StartsWithZeros_NoMoreZeros()
+        {
+            var nums = new int[10] { 0, 1, 8, 2, 3, 10, 4, 5, 30, 50 };
+            new DuplicateZeros().Go(nums);
+
+            var result = new int[10] { 0, 0, 1, 8, 2, 3, 10, 4, 5, 30 };
+            Assert.Equal(nums, result);
+        }
+
+
+        [Fact]
+        public void EdgeTest_StartsAllZeros()
+        {
+            var nums = new int[10] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            new DuplicateZeros().Go(nums);
+
+            var result = new int[10] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            Assert.Equal(nums, result);
+        }
+
+        [Fact]
+        public void EdgeTest_ManyInitialZeros()
+        {
+            var nums = new int[10] { 0, 0, 0, 0, 4, 8, 0, 0, 0, 0 };
+            new DuplicateZeros().Go(nums);
+
+            var result = new int[10] { 0, 0, 0, 0, 0, 0, 0, 0, 4, 8 };
+            Assert.Equal(nums, result);
+        }
+        
+        [Fact]
+        public void EdgeTest_ManyInitialZeros2()
+        {
+            var nums = new int[10] { 0, 0, 0, 0, 4, 0, 1, 0, 0, 0 };
+            new DuplicateZeros().Go(nums);
+
+            var result = new int[10] { 0, 0, 0, 0, 0, 0, 0, 0, 4, 0 };
+            Assert.Equal(nums, result);
+        }
+            
     }
 }
